3b3db3244d39fbcc5412ad6b9a6e76b9527e4087
RodrigoOliveiraXavier <rodrigo.oliveira.xavier151@gmail.com>
2021-12-18 19:32:28 -0300

    Finalizado layout HTML e CSS

---
+++ includes/navbar.php
@@ -0,0 +1,5 @@
+<nav class="navbar">
+  <a class="navbar-brand" href="#" style="padding-left: 2rem;">
+    <img src="logoduo.png" height="40px" alt="logo-duo">
+  </a>
+</nav>
--- views/HomeView.php
+++ views/HomeView.php
@@ -1,46 +1,45 @@
 <?php
-include_once(__PATH__ . '/includes/header.php');
+require_once(__PATH__ . '/includes/header.php');
+require_once(__PATH__ . '/includes/navbar.php');
 ?>
 
-<body>
-  <h2 style="text-align: center;">Teste vaga - DUO STUDIO</h2>
-  <hr />
-  <div class="container" align="center">
-    <div class="table-responsive-lg">
-      <table class="table table-hover" width="40%" border="1">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col"><b>Instituição / Nome da Capacitação</b></th>
-            <th scope="col"><b>Latitude</b></th>
-            <th scope="col"><b>Longitude</b></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          $count = 0;
-          foreach ($arrayJson as $key => $json) {
-            $dados = json_decode($json);
+<div class="container" align="center">
+  <div class="table-responsive-lg">
+    <table class="table table-hover" width="40%" border="1">
+      <thead id="head-table">
+        <tr id='head-row'>
+          <th scope="col">#</th>
+          <th scope="col"><b>Instituição / Nome da Capacitação</b></th>
+          <th scope="col"><b>Latitude</b></th>
+          <th scope="col"><b>Longitude</b></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $count = 1;
+        foreach ($arrayJson as $key => $json) {
+          $dados = json_decode($json);
 
-            $identificador = $dados->resposta_text;
-            $latitude = $dados->latitude;
-            $longitude = $dados->longitude;
+          $identificador = $dados->resposta_text;
+          $latitude = $dados->latitude;
+          $longitude = $dados->longitude;
 
-            $count++;
-            echo "
-              <tr>
-                <th id='row_collumn' scope='row'>$count</th>
+          echo "
+              <tr id='row'>
+                <th id='collumn' scope='row'>$count</th>
                 <td>$identificador</td>
                 <td>$latitude</td>
                 <td>$longitude</td>
               </tr>
-            ";
-          }
-          ?>
-        </tbody>
-      </table>
-    </div>
+          ";
+          $count++;
+        }
+        ?>
+      </tbody>
+    </table>
   </div>
-</body>
+</div>
 
-</html>
+<?php
+require_once(__PATH__ . '/includes/footer.php');
+?>
